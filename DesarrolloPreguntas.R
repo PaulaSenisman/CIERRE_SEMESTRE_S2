7c6b7b66ffdd45a1151160dc5c317b99b725a639
Paula Quinteros <paula.senisman@gmail.com>
2019-12-26 00:30:38 -0300

    Avance 2

--- DesarrolloPreguntas.R
+++ DesarrolloPreguntas.R
@@ -2,8 +2,9 @@
 ejemplos = sample(c("positivo", "negativo", "neutros"), 100, replace = TRUE)
 #Se utiliza lenght para determinar la dimensión del vector corroborando que existan 100 elementos en la lista de la variable.
 length(ejemplos)
-#Se observa como se generan los números aleatorios.
-sample(c("positivo", "negativo", "neutros"), 100, replace = TRUE)
 #Ejercicio 1:
-length(ejemplos[["positivo"]])
-
+#Se utiliza el comando table con el fin de realizar una tabulación de datos con su frecuencia asociada a los elementos aleatorios generados.
+table(ejemplos)
+#Ejercicio 2:
+set.seed(10)
+ejemplos = sample(c("positivo","negativo","neutros"), 10, replace = TRUE)
